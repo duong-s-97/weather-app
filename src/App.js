d1044d352d36b0c82f092370af378de4f98b79b2
duong-s-97 <duongdev97@gmail.com>
2024-04-18 16:12:51 +0700

    fix eslint

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
 
   useEffect(() => {
     mutationGetlocation("hanoi");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleChangeInput = (event) => {
